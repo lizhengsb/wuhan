0a59be8978bff4434ba4199f80004fe472e9f24d
lizhengsb <2539852339@qq.com>
2018-08-25 16:28:33 +0800

    加密

---
+++ src/aa/a.java
@@ -0,0 +1,11 @@
+package aa;
+
+public class a {
+	public static void main(String[] args) {
+		MD5 d=new MD5();
+		String b="ddd";
+		byte[] a=b.getBytes();
+		
+		System.out.println(d.getMD5(a));
+	}
+}
